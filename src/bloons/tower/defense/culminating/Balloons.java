db4a84adc10f3a4512d1d2639bde0860e3b62993
Sheikh Mahfuzur Rahman <56134792+smrahman1@users.noreply.github.com>
2022-01-04 21:00:46 -0500

    Cleaned up formatting

--- src/bloons/tower/defense/culminating/Balloons.java
+++ src/bloons/tower/defense/culminating/Balloons.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bloons.tower.defense.culminating;
 
 import java.util.ArrayList;
@@ -18,11 +13,11 @@ import org.newdawn.slick.SlickException;
  */
 public class Balloons extends BasicGame {
 
-    static ArrayList<Image> ball = new ArrayList();
-    
-    static float [] x = new float [20];
-      static float [] y = new float [21];
-      static int [] direction = new int [20];
+    static ArrayList < Image > ball = new ArrayList();
+
+    static float[] x = new float[20];
+    static float[] y = new float[21];
+    static int[] direction = new int[20];
     public Balloons(String title) {
         super(title);
     }
@@ -45,102 +40,102 @@ public class Balloons extends BasicGame {
     static void round1() {
         for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 0, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round2() {
-Balloons.ball.clear();
-        for (int counter = 0; counter<20;counter++){
-        x[counter]=0;
-        y[counter]=263;
-         direction[counter]=2;
-             
-                ball.add(Resources.getSpriteImage("RealBalloons", 1, 0));
-            }
-        
+        Balloons.ball.clear();
+        for (int counter = 0; counter < 20; counter++) {
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
+
+            ball.add(Resources.getSpriteImage("RealBalloons", 1, 0));
+        }
+
     }
 
     static void round3() {
         ball.clear();
-         for (int counter = 0; counter < 20; counter++) {
+        for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 2, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round4() {
- ball.clear();
+        ball.clear();
         for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 3, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round5() {
- ball.clear();
-         for (int counter = 0; counter < 20; counter++) {
+        ball.clear();
+        for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 4, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round6() {
- ball.clear();
+        ball.clear();
         for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 5, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round7() {
- ball.clear();
-         for (int counter = 0; counter < 20; counter++) {
+        ball.clear();
+        for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 6, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round8() {
- ball.clear();
-       for (int counter = 0; counter < 20; counter++) {
+        ball.clear();
+        for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 3, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round9() {
- ball.clear();
-         for (int counter = 0; counter < 20; counter++) {
-            ball.add(Resources.getSpriteImage("RealBalloons",2, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+        ball.clear();
+        for (int counter = 0; counter < 20; counter++) {
+            ball.add(Resources.getSpriteImage("RealBalloons", 2, 0));
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
     }
 
     static void round10() {
- ball.clear();
-         for (int counter = 0; counter < 20; counter++) {
+        ball.clear();
+        for (int counter = 0; counter < 20; counter++) {
             ball.add(Resources.getSpriteImage("RealBalloons", 1, 0));
-            x[counter]=0;
-            y[counter]=263;
-             direction[counter]=2;
+            x[counter] = 0;
+            y[counter] = 263;
+            direction[counter] = 2;
         }
 
-}
+    }
 }
